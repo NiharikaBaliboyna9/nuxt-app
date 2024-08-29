fa91ec28bfe1dcf252ee654219a3bb8d6b8034a8
NiharikaBaliboyna9 <balibon@amazon.com>
2024-08-29 20:28:23 +0530

    Update hello.ts

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,4 +1,17 @@
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
+    try{
+        
+        const url= 'https://jsx63ciwv4.execute-api.us-west-2.amazonaws.com/v1/users'
+        const response= await fetch(url,{
+            method:'GET'
+        });
+        console.log(response)
+        return response;
+
+    }
+    catch(error){
+
+    }
     console.log('server route requested')
     return {
       hello: "world",
